d65416a361973a79529f09436604e748e018bc5a
Dragonlord1005 <68302450+Dragonlord1005@users.noreply.github.com>
2023-03-27 16:32:21 -0600

    ✨ Now can add infinite episodes

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
-
-import spotipy
 from dotenv import load_dotenv
+import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
 # Load environment variables from .env file
@@ -17,20 +16,23 @@ username = os.getenv('SPOTIPY_USERNAME')
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id,
                                                client_secret=client_secret,
                                                redirect_uri=redirect_uri,
-                                               scope='playlist-modify-public, user-library-modify'))  # noqa: E501
+                                               scope='playlist-modify-public, playlist-modify-private, user-library-read, user-library-modify'))
 
 
 # Retrieve all r/Maliciouscompliance episodes
 show_id = 'spotify:show:3hJo9o8qFqrblXu1Plkv8L'
 episode_uris = []
-for offset in range(5):
-    results = sp.show_episodes(show_id=show_id, market='US', limit=50, offset=offset*50)
+
+for offset in range(501):
+    results = sp.show_episodes(show_id=show_id, market='US', limit=50, offset=offset)
+    
+    # Print out what offset we're on
+    print("We're on offset {}".format(offset))
+
+    # Filter out non r/Maliciouscompliance episodes and retrieve their URIs
     for item in results['items']:
         if item['name'].startswith('r/Maliciouscompliance'):
-            episode_uri = item['uri']
-            # Check if episode URI is already in the playlist
-            if episode_uri not in episode_uris:
-                episode_uris.append(episode_uri)
+            episode_uris.append(item['uri'])
 
 # Check if playlist already exists, and create it if it doesn't
 playlist_name = 'r/MaliciousCompliance Episodes'
@@ -48,13 +50,16 @@ if not playlist_exists:
 # Add episodes to playlist, without creating duplicates
 existing_tracks = sp.playlist_tracks(playlist_id)['items']
 existing_track_uris = [track['track']['uri'] for track in existing_tracks]
-new_track_uris = [episode_uri for episode_uri in episode_uris if episode_uri not in existing_track_uris]  # noqa: E501
+new_track_uris = [episode_uri for episode_uri in episode_uris if episode_uri not in existing_track_uris]
 if new_track_uris:
-    sp.playlist_add_items(playlist_id, new_track_uris)
+    # Split new_track_uris into batches of up to 100 and add each batch individually
+    for i in range(0, len(new_track_uris), 100):
+        batch = new_track_uris[i:i+100]
+        sp.playlist_add_items(playlist_id, batch)
 
 # Sort playlist by oldest to newest
 # TODO: Fix not sorting correctly, or at all for that matter
 # Sort playlist by oldest to newest
 playlist_length = len(existing_tracks) + len(new_track_uris)
 if playlist_length > 1:
-    sp.playlist_reorder_items(playlist_id, range_start=0, insert_before=1, range_length=playlist_length)  # noqa: E501
+    sp.playlist_reorder_items(playlist_id, range_start=0, insert_before=1, range_length=playlist_length)
